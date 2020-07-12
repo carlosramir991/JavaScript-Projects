80f09eaa2305c42641ac6dc32c3a7d34c4bdaf4f
carlosramir991 <65266113+carlosramir991@users.noreply.github.com>
2020-07-11 19:52:36 -0600

    Add files via upload

---
+++ JS/JavaScript_File.js
@@ -0,0 +1,32 @@
+
+
+var name="Carlos Ramirez ";
+
+var creation="made this.";
+
+var red = " Jeffrey Epstein didn't kill himself. ";
+//He really didn't//
+
+var red = red.fontcolor("red");
+
+window.alert("Welcome. ");
+
+document.write(name+creation+ red);
+
+//This part of the code is for calculating the circumference of a circle//
+var r = 2.5; 
+
+var pi = 3.141;
+
+var y = 2; 
+
+var z = (r * pi * y);
+
+
+
+document.write("If the radius of a cricle is 2.5 then the circumference is equal to: " + z);
+
+
+
+
+
