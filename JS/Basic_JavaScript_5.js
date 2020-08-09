f6c011686c1e483362b681f452bed36ef41c8eac
carlosramir991 <65266113+carlosramir991@users.noreply.github.com>
2020-08-09 15:55:04 -0600

    Add files via upload

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,42 @@
+document.write(typeof 5);
+
+document.write(-3e308);
+
+document.write(3e308);
+
+document.write(10>2);
+
+document.write(4<2);
+
+console.log(8 * 8);
+
+document.write("The number PI is "+3.141);
+
+console.log(10>20);
+
+document.write(10==9+1);
+
+document.write(10==9);
+
+document.write(10===10);
+
+document.write("penguin"===10);
+
+document.write("ten"===10);
+
+document.write("ten"==="nine");
+
+document.write(9===9 && 10===10 );
+
+document.write(9==="nine" && 10===10);
+
+document.write(9===9 || 8>10);
+
+document.write(9==="nine"||8==="eight");
+
+function not_func() {
+    document.getElementById("notFunct").innerHTML=!(10>2);
+}
+
+
+
